6876da060cf1f35b5c991d7120b5984938b65adb
Sammy <1449@holbertonschool.com>
2020-03-21 20:49:25 +0000

    update profile

---
+++ conexions/edit_profile.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+    $id_user = $_SESSION["Id_user"];
+    include_once("connect.php");
+    $name = $_POST["name"];
+    $lastname = $_POST["last_name"];
+    $pass = $_POST["passw"];
+    if ($pass != ""){
+        $update = "UPDATE fionadb.users
+        SET name='$name', last_name='$lastname', password='$pass'
+        WHERE id_user='$id_user';";
+    } else{
+        $update = "UPDATE fionadb.users
+        SET name='$name', last_name='$lastname'
+        WHERE id_user='$id_user';";
+    }
+    $save = mysqli_query($conn, $update);
+    if(!$save){ 
+        echo 400;
+    } else {
+        echo 200;
+        $_SESSION["name"] = $name;
+        $_SESSION["last_name"] = $lastname;
+    }
+    mysqli_close($conn);
+?>
