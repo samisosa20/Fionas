ec94e6f5ed96f353fd72768aa95154d79f129e9d
Sammy <1449@holbertonschool.com>
2020-03-25 15:33:33 +0000

    add new column in the insert

--- conexions/add_account.php
+++ conexions/add_account.php
@@ -6,9 +6,10 @@
     $descripcion = $_POST["descripcion"];
     $divisa = $_POST["divisa"];
     $monto_ini = $_POST["monto_ini"];
+    $acco_save = $_POST["acco_save"];
     $insert = "INSERT INTO fionadb.cuentas
-    (nombre, descripcion, Divisa, monto_inicial, id_user)
-    VALUES('$nombre', '$descripcion', '$divisa', $monto_ini, '$id_user');    
+    (nombre, descripcion, Divisa, monto_inicial, cuenta_ahorro, id_user)
+    VALUES('$nombre', '$descripcion', '$divisa', $monto_ini, $acco_save, '$id_user');    
     ";
     $save = mysqli_query($conn, $insert);
     if(!$save){ 
