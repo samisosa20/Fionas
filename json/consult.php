795474775c1624edc281b7ed221664da9c7dde1f
Sammy <1449@holbertonschool.com>
2020-03-25 13:06:52 +0000

    changes consult to don't show Transferencias

--- json/consult.php
+++ json/consult.php
@@ -9,7 +9,7 @@ function list_cate(){
         $lvl = $_GET['lvl'];
         $strsql = "SELECT a.id, a.categoria, COUNT(b.id) AS cantidad FROM fionadb.categorias AS a
         LEFT JOIN fionadb.categorias AS b ON (a.id = b.sub_categoria) WHERE a.id_user='$id_user'
-        and a.sub_categoria=$lvl GROUP BY a.categoria";
+        and a.sub_categoria=$lvl and a.categoria != 'Transferencia' GROUP BY a.categoria";
         $rs = mysqli_query($conn, $strsql);
         $total_rows = $rs->num_rows;
         if ($total_rows > 0 ) {
