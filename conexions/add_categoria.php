3d35b7ef8cfd395fb397289ffb78e099bfec24e6
samisosa20 <1449@holbertonschool.com>
2020-04-17 16:00:44 +0000

    change to can selected father category

--- conexions/add_categoria.php
+++ conexions/add_categoria.php
@@ -6,10 +6,19 @@
     $descripcion = $_POST["descripcion"];
     $grupo = $_POST["grupo"];
     $categoria = $_POST["categoria"];
-    $insert = "INSERT INTO fionadb.categorias
-    (categoria, sub_categoria, descripcion, grupo, id_user)
-    VALUES('$nombre', '$categoria', '$descripcion', '$grupo', '$id_user');
-    ";
+    if ($categoria == 0){
+        $insert = "INSERT INTO fionadb.categorias
+        (categoria, sub_categoria, descripcion, grupo, id_user)
+        SELECT '$nombre', `AUTO_INCREMENT` AS sub, '$descripcion', '$grupo', '$id_user'
+        FROM  INFORMATION_SCHEMA.TABLES
+        WHERE TABLE_SCHEMA = 'fionadb'
+        AND   TABLE_NAME   = 'categorias';";
+    } else {
+        $insert = "INSERT INTO fionadb.categorias
+        (categoria, sub_categoria, descripcion, grupo, id_user)
+        VALUES('$nombre', '$categoria', '$descripcion', '$grupo', '$id_user');
+        ";
+    }
     $save = mysqli_query($conn, $insert);
     if(!$save){ 
         echo 400;
